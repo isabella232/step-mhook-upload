7661baa73b43098bb653eb37019a6aa512584607
Matthew Hooker <mwhooker@gmail.com>
2016-02-01 19:21:08 -0800

    testing

--- run.sh
+++ run.sh
@@ -27,12 +27,22 @@ fi
 export AWS_ACCESS_KEY_ID=$WERCKER_MHOOK_UPLOAD_KEY_ID
 export AWS_SECRET_ACCESS_KEY=$WERCKER_MHOOK_UPLOAD_KEY_SECRET
 
-
-./mhook upload \
-    --bucket $WERCKER_MHOOK_UPLOAD_BUCKET \
+echo ./mhook upload --bucket $WERCKER_MHOOK_UPLOAD_BUCKET \
     --project $WERCKER_GIT_REPOSITORY \
     --branch $WERCKER_GIT_BRANCH \
     --commit $WERCKER_GIT_COMMIT \
     $UPLOAD_LATEST_FLAG \
     $WERCKER_MHOOK_UPLOAD_SOURCE \
     $WERCKER_MHOOK_UPLOAD_PREFIX
+
+./mhook upload --bucket $WERCKER_MHOOK_UPLOAD_BUCKET \
+    --project $WERCKER_GIT_REPOSITORY \
+    --branch $WERCKER_GIT_BRANCH \
+    --commit $WERCKER_GIT_COMMIT \
+    $UPLOAD_LATEST_FLAG \
+    $WERCKER_MHOOK_UPLOAD_SOURCE \
+    $WERCKER_MHOOK_UPLOAD_PREFIX
+
+if [ $? -ne 0 ]; then
+    fail 'mhook upload command failed.'
+fi
